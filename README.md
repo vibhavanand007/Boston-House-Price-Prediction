e1218a292da97c1f9a5d93aba22c072c88c96667
Vibhav <vibhavanand25@gmail.com>
2024-09-20 13:46:09 +0530

    This is the commit for Web Application

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 ### End to End project
 
 Boston House Pricing Prediction --[Linear Regression]
+[Live Link]()
+
+Create a new environment
+
+```
+conda create -p venv python==3.11.7 -y
+```
---
+++ app.py
@@ -0,0 +1,29 @@
+import pickle
+from flask import Flask, request, app, jsonify, render_template, url_for
+import numpy as np
+import pandas as pd
+
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+scalar = pickle.load(open('scaling.pkl', 'rb'))
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict_api', methods=['POST'])
+def predict():
+    data = request.json['data']
+    print(data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data = scalar.transform(np.array(list(data.values())).reshape(1,-1))
+    output = model.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
